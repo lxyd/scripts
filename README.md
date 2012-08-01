1b3fb142ce95716b3ed55d9c25df69bb20add72a
Alexey Dubinin <dubininalexey@gmail.com>
2012-08-01 14:03:07 +0400

    Added detarget.user.js

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ scripts
 =======
 
 Some useful single file simple script programs written in different scripting languages
+
+
+ytd - Extract videos cached by youtube flash web player
+
+rutd - Download RTMP stream from RuTube by video web page Url
+
+detarget.user.js - Browser userscript that removes "target" attribute from "a" elements
---
+++ detarget.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name    Detarget
+// @version    1
+// @description    Removes "target" attribute from links
+// @include    http://*/*
+// @match    http://*/*
+// @author   Alexey Dubinin
+// ==/UserScript==
+
+(function(document, fn) {
+    var script = document.createElement('script');
+    script.setAttribute("type", "text/javascript");
+    script.textContent = '(' + fn + ')(window, window.document);';
+    document.body.appendChild(script); // run the script
+    document.body.removeChild(script); // clean up
+})(document, function(window, document) {
+
+    function domReady(fun) {
+        if (document.readyState === "interactive" || document.readyState === "complete") {
+            fun();
+        } else {
+            document.addEventListener("DOMContentLoaded", fun, false);
+        }
+    }
+
+    function onEvent(evt) {
+        var el = evt.srcElement || evt.originalTarget;
+
+        if (el.nodeName === 'A') {
+            el.removeAttribute('target');
+        }
+
+        return true;
+    }
+
+    domReady(function() {
+        document.addEventListener('click', onEvent);
+        document.addEventListener('focus', onEvent, true);
+        document.addEventListener('focusin', onEvent);
+    });
+
+});
