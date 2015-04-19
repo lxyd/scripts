8fd2a59ec7a93e21bdd9feb5f90bd844bd92b399
Alexey Dubinin <dubininalexey@gmail.com>
2015-04-20 00:09:30 +0300

    Version bump

--- tabun-fixes.meta.js
+++ tabun-fixes.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name    Tabun fixes
-// @version    24
+// @version    25
 // @description    Автообновление комментов, возможность выбрать формат дат, а также добавление таймлайна комментов и несколько мелких улучшений для табуна. И всё это - с графическим конфигом!
 //
 // @updateURL https://github.com/lxyd/scripts/raw/master/tabun-fixes.meta.js
